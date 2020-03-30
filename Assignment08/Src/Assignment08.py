ac7f5766ad56efd58ec3c5714f92d5164df909ce
rapienac <rapienac@LCBLAB2110-14.uccob.uc.edu>
2020-03-30 15:27:17 -0400

    My updated assignment

--- Assignment08/Src/Assignment08.py
+++ Assignment08/Src/Assignment08.py
@@ -1,4 +1,13 @@
 '''
+Andrea Rapien
+rapienac@mail.uc.edu
+Assignment 08
+IS 4010
+Spring 2020
+This project demonstrates the ability to modify a PyDev Project to process an existing spreadsheet, generate a chart, and save it in a new excel sheet.
+@author: rapienac
+'''
+'''
 Created on Mar 20, 2020
 Assignment 08 Donor Code
 Adapted from https://openpyxl.readthedocs.io/en/stable/charts/pie.html
@@ -17,11 +26,11 @@ wb = load_workbook(filename = 'Top5TransactionsByLoyaltyNumber.xlsx')
 ws = wb['Sheet1']
 
 pie = PieChart()
-labels = Reference(ws, min_col=6, min_row=2, max_row=6)
-data = Reference(ws, min_col=2, min_row=1, max_row=6)
+labels = Reference(ws, min_col=4, min_row=2, max_row=6)
+data = Reference(ws, min_col=2, min_row=2, max_row=6)
 pie.add_data(data, titles_from_data=False)
 pie.set_categories(labels)
-pie.title = "Top 5 Total Transactions by Loyalty Number"
+pie.title = "Top 5 Total Transactions by Store"
 pie.dataLabels = DataLabelList()
 pie.dataLabels.showVal = True
 
@@ -29,6 +38,6 @@ pie.dataLabels.showVal = True
 pieSlice = DataPoint(idx=0, explosion=20)
 pie.series[0].data_points = [pieSlice]
 
-ws.add_chart(pie, "A7")
+ws.add_chart(pie, "A8")
 wb.save('Top5TransactionsByLoyaltyNumberWithPieChart.xlsx') # .xlsx file cannot be open when we do this
 print("Done")
